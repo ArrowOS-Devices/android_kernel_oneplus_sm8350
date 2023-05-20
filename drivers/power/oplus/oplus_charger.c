e7e11e3a1dce02249a1b82329f72fe3ea7bc513f
danielml <daniel@danielml.dev>
2023-05-20 20:24:14 +0200

    drivers: power: oplus: Make the otg_switch read-only
    Change-Id: I80644f6e74c776cc4ee80a8502f796191b9e52b3
    Signed-off-by: danielml <daniel@danielml.dev>

--- drivers/power/oplus/oplus_charger.c
+++ drivers/power/oplus/oplus_charger.c
@@ -195,6 +195,11 @@ bool __attribute__((weak)) oplus_is_ptcrb_version(void)
 	return false;
 }
 
+void __attribute__((weak)) oplus_set_otg_switch_status(bool value)
+{
+        return;
+}
+
 /****************************************/
 #define RESET_MCU_DELAY_30S		6
 static int reset_mcu_delay = 0;
@@ -7422,6 +7427,8 @@ static void oplus_chg_variables_init(struct oplus_chg_chip *chip)
 	chip->bidirect_abnormal_adapter = false;
 	chip->cool_down_check_done = false;
 	chip->limits.force_input_current_ma = 0;
+
+	oplus_set_otg_switch_status(true);
 }
 
 static void oplus_chg_fail_action(struct oplus_chg_chip *chip)
